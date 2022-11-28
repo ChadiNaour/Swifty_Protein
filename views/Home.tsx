32734bbc755b6541081d33cb3a7adcabbfe43666
ChadiNaour <naourchadi@gmail.com>
2022-11-28 12:58:58 +0100

    Feat: adding the authentification with touch id and biometrics on pressing the login button

--- views/Home.tsx
+++ views/Home.tsx
@@ -1,11 +1,41 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-const Home = () => {
+import { StyleSheet, Text, View, TextInput } from 'react-native';
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+import type { NativeStackScreenProps } from '@react-navigation/native-stack';
+import type { RootStackParamList } from '../App';
+
+type Props = NativeStackScreenProps<RootStackParamList, 'Home'>;
+const Home = ({ navigation }: Props) => {
+    const [Search, onChangeSearch] = useState("");
+    const [fontsLoaded] = useFonts({
+        'NexaBold': require('../assets/fonts/Nexa/NexaTextDemo-Bold.ttf'),
+    });
+
+    const onLayoutRootView = useCallback(async () => {
+        if (fontsLoaded) {
+            await SplashScreen.hideAsync();
+        }
+    }, [fontsLoaded]);
+    if (!fontsLoaded) {
+        return null;
+    }
+
     return (
-        <View style={styles.container}>
-            <Text>Home</Text>
+        <View style={styles.container} onLayout={onLayoutRootView}>
             <StatusBar style="auto" />
+            <View style={styles.TopContainer}>
+                <View style={styles.SearchContainer}>
+                    <Text style={{ fontFamily: 'NexaBold', fontSize: 23, color: 'black' }}>Ligands</Text>
+                    <TextInput
+                        style={styles.input}
+                        onChangeText={onChangeSearch}
+                        value={Search}
+                        placeholder="Search"
+                    />
+                </View>
+            </View>
         </View>
     )
 }
@@ -15,8 +45,52 @@ export default Home;
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+        flexDirection: "column",
         backgroundColor: '#fff',
         alignItems: 'center',
         justifyContent: 'center',
+    },
+    TopContainer: {
+        position: 'absolute',
+        top: 0,
+        backgroundColor: "#0F0E0E",
+        height: 150,
+        width: "100%",
+        borderBottomEndRadius: 15,
+        borderBottomStartRadius: 15,
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+
+    },
+    SearchContainer: {
+        position: 'absolute',
+        top: 50,
+        backgroundColor: "white",
+        height: 150,
+        width: "85%",
+        borderRadius: 20,
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 3.84,
+
+        elevation: 5,
+    },
+    input: {
+        width: '80%',
+        margin: 12,
+        borderWidth: 1,
+        borderRadius: 10,
+        borderColor: "#DADADA",
+        backgroundColor: "F5F5F5",
+        paddingHorizontal: 15,
+        paddingVertical: 10
     },
 });
--- views/Login.tsx
+++ views/Login.tsx
@@ -1,16 +1,17 @@
 import React, { useCallback } from 'react';
-import { Pressable, Image, StyleSheet, Text, View } from 'react-native';
+import { Pressable, Image, Alert, StyleSheet, Text, View } from 'react-native';
 import { useFonts } from 'expo-font';
 import * as SplashScreen from 'expo-splash-screen';
+import * as LocalAuthentication from 'expo-local-authentication';
+import type { NativeStackScreenProps } from '@react-navigation/native-stack';
+import type { RootStackParamList } from '../App';
 
-
-SplashScreen.preventAutoHideAsync();
-
-const Login = () => {
+type Props = NativeStackScreenProps<RootStackParamList, 'Login'>;
+const Login = ({ navigation }: Props) => {
     const [fontsLoaded] = useFonts({
         'NexaBold': require('../assets/fonts/Nexa/NexaTextDemo-Bold.ttf'),
+        'MondaBold': require('../assets/fonts/Monda/Monda-Bold.ttf'),
         'NexaBold2': require('../assets/fonts/Nexa/NexaDemo-Bold.ttf'),
-        'MondaBold': require('../assets/fonts/Monda/Monda-bold.ttf'),
     });
 
     const onLayoutRootView = useCallback(async () => {
@@ -23,6 +24,26 @@ const Login = () => {
         return null;
     }
 
+    async function authenticate() {
+        try {
+            const authentTypes = await LocalAuthentication.supportedAuthenticationTypesAsync();
+            console.log("authentification types:", authentTypes);
+            if (authentTypes.includes(1) || authentTypes.includes(2)) {
+                const result = await LocalAuthentication.authenticateAsync();
+                console.log("authentification result", result);
+                if (result.success) {
+                    navigation.navigate('Home');
+                }
+            }
+            else
+                navigation.navigate('Home');
+        }
+        catch (error: any) {
+            Alert.alert("an error has occured", error.message);
+        }
+
+    }
+
     return (
         <View style={styles.container} onLayout={onLayoutRootView}>
             <View style={styles.TopContainer}>
@@ -30,7 +51,7 @@ const Login = () => {
                     style={styles.SymbolLogo}
                     source={require('../assets/images/Symbol_Logo.png')}
                 />
-                <Text style={{ position: "absolute", bottom: -75, fontFamily: 'MondaBold', fontSize: 23, color: 'black' }}>42 Proteins</Text>
+                <Text style={{ position: "absolute", bottom: -80, fontFamily: 'MondaBold', fontSize: 23, color: 'black' }}>42 Proteins</Text>
             </View>
             <View style={styles.BottomContainer}>
                 <View style={styles.learnMoreContainer}>
@@ -48,10 +69,10 @@ const Login = () => {
                         fontSize: 16,
                         fontWeight: '500',
                         textAlign: 'center'
-                    }}>SwiftyProteins is a visualizer for proteins models according to standardized representation from the famous PDB (Protein Data Bank) using SceneKit.</Text>
+                    }}>42 Proteins is a visualizer for protein models according to the standardized representation from the famous PDB (Protein Data Bank) using SceneKit.</Text>
                 </View>
                 <View style={styles.buttonContainer}>
-                    <Pressable style={styles.LoginButton}>
+                    <Pressable style={styles.LoginButton} onPress={authenticate}>
                         <Text style={{
                             fontFamily: 'NexaBold',
                             color: "white",
@@ -84,8 +105,8 @@ const styles = StyleSheet.create({
         backgroundColor: "#0F0E0E",
         height: "40%",
         width: "100%",
-        borderBottomEndRadius: 100,
-        borderBottomStartRadius: 100,
+        borderBottomEndRadius: 150,
+        borderBottomStartRadius: 150,
         display: 'flex',
         alignItems: 'center',
         justifyContent: 'center',
